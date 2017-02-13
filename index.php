e313e567526d3786ad66669caab6aaa1483f2a0b
shohan494 <shohan12224943@gmail.com>
2017-02-14 02:18:55 +0600

    initial commit, project ready with image intervention editor api

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// include composer autoload
+require 'vendor/autoload.php';
+
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManagerStatic as Image;
+
+// configure with favored image driver (gd by default)
+Image::configure(array('driver' => 'imagick'));
+
+// and you are ready to go ...
+//capturing the image
+$image = Image::make('image/img.jpg');
+$edited = Image::make('image/img.jpg');
+
+$edited->resize(800, 650);
+
+//$edited->rotate(-45);
+$image->insert($edited, 'center');
+
+$image->save('edited/img.jpg');
+echo $image->response();
+
+?>
